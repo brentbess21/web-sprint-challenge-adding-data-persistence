ac71525f5df6b99a158418f7e647ebcdb53cd2ca
brentbess21 <brentbess21@gmail.com>
2021-10-08 11:01:42 -0600

    Initial scaffolding and connecting of project GET endpoint and project model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,11 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function get() {
+    console.log('getting projects!')
+}
+
+
+module.exports = {
+    get
+}
--- api/project/router.js
+++ api/project/router.js
@@ -2,6 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
+const Project = require('./model');
+
+router.get('/', async (req, res, next)=> {
+    try {
+        const projects = await Project.get()
+    res.status(200).json(projects)
+
+    } catch (err) {
+        next(err)
+    }
+})
 
 module.exports = router;
 
