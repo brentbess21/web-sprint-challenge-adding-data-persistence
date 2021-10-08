46ef731abe49dcc7e416f79adb48a21a947f339b
brentbess21 <brentbess21@gmail.com>
2021-10-08 09:35:41 -0600

    built working server in server.js and connected it to index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require('./api/server')
+
+const port = process.env.PRORT || 5000;
+
+server.listen(port, ()=> {
+    console.log(`*** Listening on localhost:${port} ***`)
+})
