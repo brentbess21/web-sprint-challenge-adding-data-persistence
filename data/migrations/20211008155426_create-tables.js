bd7255bc3f8c910a24e4f04edd21ba432e44695b
brentbess21 <brentbess21@gmail.com>
2021-10-08 10:10:05 -0600

    Built projects table and verified with sqlite studio

--- data/migrations/20211008155426_create-tables.js
+++ data/migrations/20211008155426_create-tables.js
@@ -1,8 +1,17 @@
 
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name').notNullable()
+        table.string('project_description')
+        table.boolean('project_completed').defaultTo(false) 
+    })
+    
+    
 };
 
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema 
+    .dropTableIfExists('projects')
 };
